ea27047c2141068d44a3edad9727139e217619ac
Kneonix <81943812+smh-kneonix@users.noreply.github.com>
2023-06-08 21:58:17 +0430

    update userinfo command
    Co-Authored-By: fardin <49183858+FacelessFK@users.noreply.github.com>

--- models/commands/userinfo.js
+++ models/commands/userinfo.js
@@ -1,17 +1,49 @@
-const { SlashCommandBuilder, Guild } = require("discord.js");
+const { SlashCommandBuilder, EmbedBuilder } = require("discord.js");
 
 module.exports = {
     data: new SlashCommandBuilder()
         .setName("userinfo")
-        .setDescription("Provides information about the user.")
+        .setDescription("mention someone or use it for your self")
+        .addUserOption((option) => {
+            return option
+                .setName("user")
+                .setDescription("mention someone")
+                .setRequired(false);
+        })
         .setDMPermission(false),
+
+    // get user info (name,joinDate,avatar) and reply it
     async execute(interaction) {
-        // get user info (name,joinDate,avatar) and reply it
-        const userName = interaction.user.username;
-        const timezone = interaction.member.joinedAt.toLocaleDateString();
-        const avatarLink = interaction.user.displayAvatarURL();
-        await interaction.reply(
-            `${userName}, who joined at ${timezone}.\n${avatarLink}`
-        );
+        let userName = interaction.user;
+        let userNamed = userName.username;
+        const mention = interaction.options.getMember("user");
+        let timezone = interaction.member.joinedAt.toLocaleDateString();
+
+        // if mention any user
+        if (mention) {
+            userName = mention;
+            userNamed = mention.user.username;
+            timezone = new Date(mention.joinedTimestamp).toLocaleDateString();
+        }
+
+        // select avatar
+        const avatarLink = userName.displayAvatarURL().toString().split(".");
+        avatarLink.pop();
+        avatarLink.push("png?size=1024")
+
+        
+
+        const embedContext = new EmbedBuilder()
+            .setColor(0x96026a)
+            .setTitle(userNamed)
+            .setURL(avatarLink.join("."))
+            .setDescription(`join at: ${timezone}`)
+            .setImage(`${avatarLink.join(".")}`)
+            .setTimestamp();
+
+        await interaction.reply({
+            // ephemeral: true,
+            embeds: [embedContext],
+        });
     },
 };
